21b642ede36d1bc1a50034a759d9b4dcdc633d91
Enno Enno <14846866+enncoded@users.noreply.github.com>
2021-09-23 01:16:10 -0400

    Finished 2-11.

--- src/challenges/set_2.rs
+++ src/challenges/set_2.rs
@@ -1,23 +1,24 @@
 use crate::utils::algos::{pkcs7_pad, pkcs7_unpad};
-use crate::utils::misc::read_no_newlines;
-use crate::utils::into_bytes::from_base64;
+use crate::utils::misc::{read_no_newlines, read_lines};
+use crate::utils::into_bytes::{from_base64, from_hex};
 use crate::utils::aes::*;
 use crate::utils::xor::repeating_key_xor;
+use itertools::Itertools;
 
 // Implement PKCS#7 padding
 #[test]
 fn challenge_9() {
-    let input = b"poo";
+    let input = b"abc";
     let block_size = 8 as usize;
-    assert_eq!(pkcs7_pad(input.to_vec(), block_size), b"poo\x05\x05\x05\x05\x05".to_vec());
+    assert_eq!(pkcs7_pad(input.to_vec(), block_size), b"abc\x05\x05\x05\x05\x05".to_vec());
 
-    let input = b"poooooooo";
+    let input = b"abcdefghi";
     let block_size = 8 as usize;
-    assert_eq!(pkcs7_pad(input.to_vec(), block_size), b"poooooooo\x07\x07\x07\x07\x07\x07\x07".to_vec());
+    assert_eq!(pkcs7_pad(input.to_vec(), block_size), b"abcdefghi\x07\x07\x07\x07\x07\x07\x07".to_vec());
 
-    let input = b"poo";
+    let input = b"abc";
     let block_size = 2 as usize;
-    assert_eq!(pkcs7_pad(input.to_vec(), block_size), b"poo\x01".to_vec());
+    assert_eq!(pkcs7_pad(input.to_vec(), block_size), b"abc\x01".to_vec());
 }
 
 // Implement CBC mode
@@ -34,4 +35,16 @@ fn challenge_10() {
     // println!("{}", String::from_utf8(decrypted).unwrap());
 
     assert_eq!(input_bytes, re_encrypted);
+}
+
+#[test]
+fn challenge_11() {
+    for i in 0..=10 {
+        let line_bytes = b"A".repeat(50);
+        let (method, encrypted) = encrypt_data_random_alg(line_bytes.as_slice());
+        let duplicates = count_duplicate_blocks(encrypted.as_slice(), 16);
+        if duplicates != 0 {
+            println!("{} - found {} duplicates for {:?}", method.to_ascii_uppercase(), duplicates, line_bytes);
+        }
+    }
 }
--- src/utils/aes.rs
+++ src/utils/aes.rs
@@ -2,6 +2,28 @@ use openssl::symm::*;
 use crate::utils::xor::repeating_key_xor;
 use itertools::Itertools;
 use crate::utils::algos::{pkcs7_pad, pkcs7_unpad};
+use rand::Rng;
+
+pub fn gen_bytes(count: usize) -> Vec<u8> {
+    (0..count).map(|_| rand::thread_rng().gen::<u8>()).collect_vec()
+}
+
+pub fn encrypt_data_random_alg(input_bytes: &[u8]) -> (&str, Vec<u8>) {
+    let mut rng = rand::thread_rng();
+    let amount_to_pad = rng.gen_range(5..=10);
+    let mut padded_input: Vec<u8> = gen_bytes(amount_to_pad);
+    padded_input.extend_from_slice(input_bytes);
+    padded_input.extend(gen_bytes(amount_to_pad));
+
+    let key = gen_bytes(16);
+    if rng.gen_bool(1.0) { // ECB
+        ("ecb", aes_128_ecb_encrypt(padded_input.as_slice(), key.as_slice()))
+    } else { // CBC
+        let iv = gen_bytes(16);
+        // println!("key: {:?}, iv: {:?}", key, iv);
+        ("cbc", aes_128_cbc_encrypt(padded_input.as_slice(), key.as_slice(), iv.as_slice()))
+    }
+}
 
 pub fn aes_128_ecb_decrypt(input_bytes: &[u8], key: &[u8]) -> Vec<u8> {
     let input_len = input_bytes.len();
@@ -13,6 +35,7 @@ pub fn aes_128_ecb_decrypt(input_bytes: &[u8], key: &[u8]) -> Vec<u8> {
 }
 
 pub fn aes_128_ecb_encrypt(input_bytes: &[u8], key: &[u8]) -> Vec<u8> {
+    let input_bytes = pkcs7_pad(input_bytes.to_vec(), 16);
     let input_len = input_bytes.len();
     let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key, None).unwrap();
     let mut encrypted = vec![0u8; input_len + key.len()];
@@ -43,4 +66,9 @@ pub fn aes_128_cbc_encrypt(input_bytes: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8>
         previous_block = aes_128_ecb_encrypt(&xor_block, key);
         previous_block.clone()
     }).concat()
+}
+
+pub fn count_duplicate_blocks(input_bytes: &[u8], block_size: usize) -> usize {
+    let chunks = input_bytes.chunks(block_size);
+    chunks.clone().count() - chunks.unique().count()
 }
